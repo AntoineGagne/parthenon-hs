e14c368085cd1295b3f7303bac30b8e90d7eb2ff
Antoine Gagné <gagnantoine@gmail.com>
2022-08-07 16:53:29 -0400

    Remove some spaces in structures

--- test/Parthenon/DecoderSpec.hs
+++ test/Parthenon/DecoderSpec.hs
@@ -143,7 +143,7 @@ spec = parallel $ do
               ("b", Decoder.integer)
             ]
         )
-        "{ a = 1, b = 5 }"
+        "{a = 1, b = 5}"
         `shouldBe` Just
           ( AStruct
               ( Just

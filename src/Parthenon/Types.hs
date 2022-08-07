bc59dca948994c68a9ead9904e4fbb46285831f2
Antoine Gagné <gagnantoine@gmail.com>
2022-08-07 16:40:44 -0400

    Add JSON encoding

--- src/Parthenon/Types.hs
+++ src/Parthenon/Types.hs
@@ -1,9 +1,14 @@
+{-# LANGUAGE DeriveGeneric #-}
+
 module Parthenon.Types
   ( Athena (..),
   )
 where
 
+import Data.Aeson
+import Data.Aeson.Key (fromText)
 import Data.Text (Text)
+import GHC.Generics
 
 data Athena
   = AStruct (Maybe [(Text, Athena)])
@@ -13,4 +18,15 @@ data Athena
   | ABigInt (Maybe Integer)
   | ADouble (Maybe Double)
   | ABoolean (Maybe Bool)
-  deriving (Eq, Show)
+  deriving (Eq, Generic, Show)
+
+instance ToJSON Athena where
+  toJSON (AStruct (Just keyValues)) = object [fromText key .= toJSON value | (key, value) <- keyValues]
+  toJSON (AStruct nothing) = toJSON nothing
+  toJSON (AArray (Just values)) = toJSON $ map toJSON values
+  toJSON (AArray nothing) = toJSON nothing
+  toJSON (AInt value) = toJSON value
+  toJSON (AString value) = toJSON value
+  toJSON (ADouble value) = toJSON value
+  toJSON (ABigInt value) = toJSON value
+  toJSON (ABoolean value) = toJSON value

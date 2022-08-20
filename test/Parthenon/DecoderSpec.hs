ce15ce0f159aef6875c5fd01c7a5390e70d2a2f1
Antoine Gagné <gagnantoine@gmail.com>
2022-08-19 23:25:12 -0400

    Fix lint warning

--- test/Parthenon/DecoderSpec.hs
+++ test/Parthenon/DecoderSpec.hs
@@ -193,11 +193,10 @@ spec = parallel $ do
         "{a = Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36}"
         `shouldBe` Just
           ( AStruct
-              ( [ ( "a",
-                    AString "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"
-                  )
-                ]
-              )
+              [ ( "a",
+                  AString "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"
+                )
+              ]
           )
     it "can decode a struct with multiple fields with string that contain commas, spaces and special characters" $
       parseMaybe
